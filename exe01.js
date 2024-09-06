cdf52348cd3d2f96c1569b0cffc0d529f8f0675c
LeonardoAdami21 <lolzinhobr1000@gmail.com>
2024-09-06 09:48:44 -0300

    master, exercicio01 finalizado com sucesso.

---
+++ exe01.js
@@ -0,0 +1,19 @@
+// 1) Observe o trecho de código:
+
+// int INDICE = 12, SOMA = 0, K = 1;
+
+// enquanto K < INDICE faça
+
+// { K = K + 1; SOMA = SOMA + K;}
+
+// imprimir(SOMA);
+
+
+let INDICE = 12, SOMA = 0, K = 1;
+
+while (K < INDICE) {
+    K = K + 1;
+    SOMA = SOMA + K;
+}
+
+console.log(SOMA)
